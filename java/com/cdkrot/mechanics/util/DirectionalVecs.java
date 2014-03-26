c46222e87d47d236e1fec3ee9ec0c8d5c15995ae
Kenzie Togami <ket1999@gmail.com>
2014-03-25 22:01:50 -0700

    Rip-off of FD, makes our dirvecs match

--- java/com/cdkrot/mechanics/util/DirectionalVecs.java
+++ java/com/cdkrot/mechanics/util/DirectionalVecs.java
@@ -1,13 +1,17 @@
 package com.cdkrot.mechanics.util;
 
+import net.minecraftforge.common.util.ForgeDirection;
+
 public class DirectionalVecs {
-    public static final VecI3Base Yneg = new VecI3Base(0, -1, 0);
-    public static final VecI3Base Ypos = new VecI3Base(0, +1, 0);
-    public static final VecI3Base Zneg = new VecI3Base(0, 0, -1);
-    public static final VecI3Base Zpos = new VecI3Base(0, 0, +1);
-    public static final VecI3Base Xneg = new VecI3Base(-1, 0, 0);
-    public static final VecI3Base Xpos = new VecI3Base(+1, 0, 0);
-    public final static VecI3Base[] list = new VecI3Base[] { Yneg, Ypos, Zneg, Zpos, Xneg, Xpos };
+    public static final VecI3Base[] list = new VecI3Base[6];
+
+    static {
+        ForgeDirection[] dirs = ForgeDirection.VALID_DIRECTIONS;
+        for (int i = 0; i < dirs.length; i++) {
+            ForgeDirection d = dirs[i];
+            list[i] = new VecI3Base(d.offsetX, d.offsetY, d.offsetZ);
+        }
+    }
 
     private DirectionalVecs() {
     }
